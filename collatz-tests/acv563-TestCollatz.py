0a98ec34ca4341dd6a92a363be5e2f51d1cd0643
vohl <acvohl@gmail.com>
2016-01-30 13:03:11 -0600

    I am done. Close #10

--- collatz-tests/acv563-TestCollatz.py
+++ collatz-tests/acv563-TestCollatz.py
@@ -12,7 +12,7 @@
 # imports
 # -------
 
-from io       import StringIO
+from io import StringIO
 from unittest import main, TestCase
 
 from Collatz import collatz_read, collatz_eval, collatz_print, collatz_solve
@@ -32,25 +32,55 @@ class TestCollatz (TestCase) :
         self.assertEqual(i,  1)
         self.assertEqual(j, 10)
 
+    def test_read_zeros (self) :
+        s = "0 0\n"
+        i, j = collatz_read(s)
+        self.assertEqual(i, 0)
+        self.assertEqual(j, 0)
+
+    def test_read_extra_space (self) :
+        s = "1      10\n"
+        i, j = collatz_read(s)
+        self.assertEqual(i, 1)
+        self.assertEqual(j, 10)
+
+    def test_read_backwards (self) :
+        s = "10 1\n"
+        i, j = collatz_read(s)
+        self.assertEqual(i, 10)
+        self.assertEqual(j, 1)
+
     # ----
     # eval
     # ----
 
+    def test_eval_zero_range (self) :
+        v = collatz_eval(5, 5)
+        self.assertEqual(v, 6)
+
+    def test_cache_for_3 (self) :
+        v = collatz_eval(3, 3)
+        self.assertEqual(v, 8)
+
     def test_eval_1 (self) :
         v = collatz_eval(1, 10)
-        self.assertEqual(v, 1)
+        self.assertEqual(v, 20)
 
     def test_eval_2 (self) :
         v = collatz_eval(100, 200)
-        self.assertEqual(v, 1)
+        self.assertEqual(v, 125)
 
     def test_eval_3 (self) :
         v = collatz_eval(201, 210)
-        self.assertEqual(v, 1)
+        self.assertEqual(v, 89)
 
     def test_eval_4 (self) :
         v = collatz_eval(900, 1000)
-        self.assertEqual(v, 1)
+        self.assertEqual(v, 174)
+
+    def test_eval_1_backward (self) :
+        v = collatz_eval(10, 1)
+        self.assertEqual(v, 20)
 
     # -----
     # print
@@ -61,15 +91,43 @@ class TestCollatz (TestCase) :
         collatz_print(w, 1, 10, 20)
         self.assertEqual(w.getvalue(), "1 10 20\n")
 
+    def test_print_random (self) :
+        w = StringIO()
+        collatz_print(w, "string", 1.1, 20)
+        self.assertEqual(w.getvalue(), "string 1.1 20\n")
+
+    def test_print_zeros (self) :
+        w = StringIO()
+        collatz_print(w, 0, 0, 0)
+        self.assertEqual(w.getvalue(), "0 0 0\n")
+
     # -----
     # solve
     # -----
 
-    def test_solve (self) :
-        r = StringIO("1 10\n100 200\n201 210\n900 1000\n")
+    def test_solve_all_ones (self) :
+        r = StringIO("5 5\n3 3\n1 1\n")
         w = StringIO()
         collatz_solve(r, w)
-        self.assertEqual(w.getvalue(), "1 10 1\n100 200 1\n201 210 1\n900 1000 1\n")
+        self.assertEqual(w.getvalue(), "5 5 6\n3 3 8\n1 1 1\n")
+
+    def test_solve (self) :
+        r = StringIO("1 10\n1 10\n100 200\n201 210\n900 1000\n")
+        w = StringIO()
+        collatz_solve(r, w)
+        self.assertEqual(w.getvalue(), "1 10 20\n1 10 20\n100 200 125\n201 210 89\n900 1000 174\n")
+
+    def test_solve_backwards (self) :
+        r = StringIO("10 1\n200 100\n210 201\n1000 900\n")
+        w = StringIO()
+        collatz_solve(r, w)
+        self.assertEqual(w.getvalue(), "10 1 20\n200 100 125\n210 201 89\n1000 900 174\n")
+
+    def test_solve_large_gap (self) :
+        r = StringIO("162 1000\n162 5000\n")
+        w = StringIO()
+        collatz_solve(r, w)
+        self.assertEqual(w.getvalue(), "162 1000 179\n162 5000 238\n")
 
 # ----
 # main
